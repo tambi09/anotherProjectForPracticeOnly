412f1100eb40fab79b04cfd14ad527c916a8bf73
tambi09 <ritylya09@gmail.com>
2021-06-02 23:50:30 -0400

    day 3 added2

--- src/main/java/day3_practice/work3.java
+++ src/main/java/day3_practice/work3.java
@@ -4,5 +4,6 @@ public class work3 {
     public static void main(String[] args) {
         System.out.println("day 3 project started");
         System.out.println(" added");
+        System.out.println("added");
     }
 }
