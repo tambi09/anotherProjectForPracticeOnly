d05342acbd62d1b6c86ad5fbcc496ac3f697f3b3
tambi09 <ritylya09@gmail.com>
2021-06-02 23:50:15 -0400

    day 3 added

--- src/main/java/day3_practice/work3.java
+++ src/main/java/day3_practice/work3.java
@@ -2,6 +2,7 @@ package day3_practice;
 
 public class work3 {
     public static void main(String[] args) {
-        System.out.println("da3 project started");
+        System.out.println("day 3 project started");
+        System.out.println(" added");
     }
 }
