359321e879c944c16909376f6f6b822c70895e76
tambi09 <ritylya09@gmail.com>
2021-06-02 23:56:26 -0400

    day 3 added new class

---
+++ src/main/java/day3_practice/fromLocal.java
@@ -0,0 +1,11 @@
+package day3_practice;
+
+public class fromLocal {
+    public static void main(String[] args) {
+        System.out.println("day 3 project started");
+        System.out.println(" added");
+        System.out.println("added");
+        System.out.println("added1");
+        System.out.println("done");
+    }
+}
